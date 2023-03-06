9e4a3d69a589336eba531a2b5ea9818639f62e9a
Jeef-Moreira <119500249+Jeef-Moreira@users.noreply.github.com>
2023-03-06 11:24:46 -0300

    feat: adicionado as rotas dos 3 endpoints + a rota post da aplicação

--- visao-computacional/handler.py
+++ visao-computacional/handler.py
@@ -1,4 +1,9 @@
 import json
+import boto3
+from datetime import datetime
+
+s3 = boto3.client("s3")
+rekognition = boto3.client("rekognition")
 
 
 def health(event, context):
@@ -11,20 +16,47 @@ def health(event, context):
 
     return response
 
+
 def v1_description(event, context):
-    body = {
-        "message": "VISION api version 1."
-    }
+    body = {"message": "VISION api version 1."}
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
     return response
 
+
 def v2_description(event, context):
-    body = {
-        "message": "VISION api version 2."
-    }
+    body = {"message": "VISION api version 2."}
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
+    return response
+
+
+def post_vision(event, context):
+    # Get the bucket and image name from the request body
+    bucket_name = event["body"]["bucket"]
+    image_name = event["body"]["imageName"]
+    # Get the current datetime
+    current_datetime = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+    # Construct the URL to the image
+    url_to_image = f"https://{bucket_name}.s3.amazonaws.com/{image_name}"
+    # Call Rekognition to get labels for the image
+    response = rekognition.detect_labels(
+        Image={"S3Object": {"Bucket": bucket_name, "Name": image_name}},
+        MaxLabels=10,
+        MinConfidence=90,
+    )
+    # Get the labels from the response
+    labels = response["Labels"]
+    # Construct the response body
+    body = {
+        "url_to_image": url_to_image,
+        "created_image": current_datetime,
+        "labels": labels,
+    }
+    # Print the response body
+    print(body)
+    # Return the response
+    response = {"statusCode": 200, "body": json.dumps(body)}
     return response
