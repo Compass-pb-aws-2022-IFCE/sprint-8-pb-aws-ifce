42937cd7a89c6a1b17c0c750e0aa5fe81c74fbce
Jeef-Moreira <119500249+Jeef-Moreira@users.noreply.github.com>
2023-03-07 18:46:48 -0300

    test: testando a rota v3/vision porém estar com erro ao apurar o EMOTIONS como parâmetro de "Attributes"

--- visao-computacional/handler.py
+++ visao-computacional/handler.py
@@ -24,59 +24,127 @@ def v2_description(event, context):
     return response
 
 def post_vision(event, context):
-    # Get the bucket and image name from the request body
     body = json.loads(event["body"])
     bucket_name = body["bucket"]
     image_name = body["imageName"]
-    # Get the current datetime
+    
     current_datetime = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-    # Construct the URL to the image
+    
     url_to_image = f"https://{bucket_name}.s3.amazonaws.com/{image_name}"
-    # Call Rekognition to get labels for the image
+    
     response = rekognition.detect_labels(
         Image={"S3Object": {"Bucket": bucket_name, "Name": image_name}},
         MaxLabels=10,
         MinConfidence=90,
     )
-    # Get the labels from the response
+    
     labels = response["Labels"]
-    # Construct the response body
+    
     body = {
         "url_to_image": url_to_image,
         "created_image": current_datetime,
         "labels": labels,
     }
-    # Print the response body
+    
     print(body)
-    # Return the response
+    
     response = {"statusCode": 200, "body": json.dumps(body)}
     return response
 
 def v2_vision(event, context):
-    # Get the bucket and image name from the request body
+    
     body = json.loads(event["body"])
     bucket_name = body["bucket"]
     image_name = body["imageName"]
-    # Get the current datetime
-    current_datetime = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-    # Construct the URL to the image
+
+    
     url_to_image = f"https://{bucket_name}.s3.amazonaws.com/{image_name}"
-    # Call Rekognition to get labels for the image
-    response = rekognition.detect_labels(
+
+    
+    response = rekognition.detect_faces(
         Image={"S3Object": {"Bucket": bucket_name, "Name": image_name}},
-        MaxLabels=10,
-        MinConfidence=90,
+        Attributes=["ALL"]
     )
-    # Get the labels from the response
-    labels = response["Labels"]
-    # Construct the response body
+    
+    have_faces = len(response['FaceDetails']) > 0
+
+    position_faces = []
+    if have_faces:
+        for face_detail in response['FaceDetails']:
+            position_faces.append({
+                'Width': face_detail['BoundingBox']['Width'],
+                'Height': face_detail['BoundingBox']['Height'],
+                'Left': face_detail['BoundingBox']['Left'],
+                'Top': face_detail['BoundingBox']['Top']
+            })
+    else:
+        position_faces = None
+
+    current_datetime = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+
     body = {
         "url_to_image": url_to_image,
         "created_image": current_datetime,
-        "labels": labels,
+        "have_faces": have_faces,
+        "position_faces": position_faces
     }
-    # Print the response body
+
     print(body)
-    # Return the response
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+    return response
+
+def v3_vision(event, context):
+    body = json.loads(event["body"])
+    bucket_name = body["bucket"]
+    image_name = body["imageName"]
+
+    url_to_image = f"https://{bucket_name}.s3.amazonaws.com/{image_name}"
+
+    response = rekognition.detect_faces(
+        Image={"S3Object": {"Bucket": bucket_name, "Name": image_name}},
+        Attributes=["ALL"]
+    )
+    emotion_response = rekognition.detect_faces(
+        Image={"S3Object": {"Bucket": bucket_name, "Name": image_name}},
+        Attributes=["EMOTIONS"]
+    )
+
+    have_faces = len(response['FaceDetails']) > 0
+
+    position_faces = []
+    if have_faces:
+        for face_detail in response['FaceDetails']:
+            position_faces.append({
+                'Width': face_detail['BoundingBox']['Width'],
+                'Height': face_detail['BoundingBox']['Height'],
+                'Left': face_detail['BoundingBox']['Left'],
+                'Top': face_detail['BoundingBox']['Top']
+            })
+    else:
+        position_faces = None
+    
+    have_emotions = len(emotion_response['FaceDetails']) > 0
+
+    classified_emotion = None
+    classified_emotion_confidence = None
+    if have_emotions:
+        main_emotion = max(emotion_response['FaceDetails'][0]['Emotions'], key=lambda x: x['Confidence'])
+        classified_emotion = main_emotion['Type']
+        classified_emotion_confidence = main_emotion['Confidence']
+
+    current_datetime = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+
+    body = {
+        "url_to_image": url_to_image,
+        "created_image": current_datetime,
+        "have_faces": have_faces,
+        "position_faces": position_faces,
+        "classified_emotion": classified_emotion,
+        "classified_emotion_confidence": classified_emotion_confidence
+    }
+
+    print(body)
+
     response = {"statusCode": 200, "body": json.dumps(body)}
     return response
