1400a09c238110b996fdc328d4ccea2bb4a201fb
Humbert010 <humbertosampaio.dev@gmail.com>
2023-03-10 11:52:50 -0300

    refactor: rota v3/vision retornando posições

--- visao-computacional/routes/v3.py
+++ visao-computacional/routes/v3.py
@@ -18,20 +18,39 @@ def v3_vision(event, context):
         error_message = str(e)
         return {"statusCode": 500, "body": json.dumps({"error": error_message})}
     
-    # Cria uma lista com as emoções de todos os rostos detectados
-    emotions = []
-    for face in response_faces['FaceDetails']:
-        highest_emotion = max(face['Emotions'], key=lambda e: e['Confidence'])
-        emotions.append({
-            "type": highest_emotion['Type'].upper(),
-                "confidence": highest_emotion['Confidence']
+    # Cria a lista de rostos detectados com as emoções classificadas
+    faces = []
+    if not response_faces['FaceDetails']:
+        # Adiciona um objeto de rosto vazio com valores nulos
+        faces.append({
+            "position": {
+                "Height": None,
+                "Left": None,
+                "Top": None,
+                "Width": None
+            },
+            "classified_emotion": None,
+            "classified_emotion_confidence": None
         })
+    else:
+        for face in response_faces['FaceDetails']:
+            highest_emotion = max(face['Emotions'], key=lambda e: e['Confidence'])
+            faces.append({
+                "position": {
+                    "Height": face['BoundingBox']['Height'],
+                    "Left": face['BoundingBox']['Left'],
+                    "Top": face['BoundingBox']['Top'],
+                    "Width": face['BoundingBox']['Width']
+                },
+                "classified_emotion": highest_emotion['Type'],
+                "classified_emotion_confidence": highest_emotion['Confidence']
+            })
 
     # Cria o objeto de resposta
     response_data = {
         "url_to_image": f"https://{bucket}/{image_name}",
         "created_image": get_image_creation_date(bucket, image_name),
-        "classified_emotions": emotions
+        "faces": faces
     }
 
     # Mostra a resposta no CloudWatch:
